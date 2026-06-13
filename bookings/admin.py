5a28833d19f29736914400ba785282847f93a28e
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-13 21:59:13 +0800

    admin fixed

--- bookings/admin.py
+++ bookings/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Property, Booking
 
 
-admin.site.register(Property)
-admin.site.register(Booking)
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "property",
+        "contract_sent",
+        "contract_signed",
+        "created_at",
+    )
+
+    list_filter = (
+        "contract_sent",
+        "contract_signed",
+    )
