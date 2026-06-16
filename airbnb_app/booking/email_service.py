4b1be688211c7d38f5fd98cc515d67c07e358f61
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-16 17:47:34 +0800

    updated email_service I added contracts

--- airbnb_app/booking/email_service.py
+++ airbnb_app/booking/email_service.py
@@ -7,12 +7,19 @@ resend.api_key = settings.RESEND_API_KEY
 def send_admin_notification(booking):
     return resend.Emails.send({
         "from": "onboarding@resend.dev",
-        "to": ["karlwernerartates@gmail.com"],  # <-- YOU
+        "to": ["karlwernerartates@gmail.com"],
         "subject": "New Airbnb Booking",
+
         "html": f"""
             <h2>New Booking Received</h2>
-            <p><b>Guest Email:</b> {booking.email}</p>
-            <p><b>Property:</b> {booking.property.name}</p>
-            <p><b>Date:</b> {booking.created_at}</p>
+
+            <p><strong>Guest Email:</strong> {booking.email}</p>
+            <p><strong>Property:</strong> {booking.property.name}</p>
+            <p><strong>Created At:</strong> {booking.created_at}</p>
+
+            <hr>
+
+            <p><strong>Contract Sent:</strong> {booking.contract_sent}</p>
+            <p><strong>Contract Signed:</strong> {booking.contract_signed}</p>
         """
     })
