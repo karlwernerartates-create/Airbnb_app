360157dbe5cc614f977aa7bfe0fc5a591ccdc599
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-16 17:48:29 +0800

    added display inside admin

--- airbnb_app/booking/admin.py
+++ airbnb_app/booking/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Property, Booking
 
 admin.site.register(Property)
-admin.site.register(Booking)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "property",
+        "contract_sent",
+        "contract_signed",
+        "created_at",
+    )
