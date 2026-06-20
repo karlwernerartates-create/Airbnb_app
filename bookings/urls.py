8148a07914e3651098f88d2be9626349e3434c41
George Williamson <gbwilliamson4@gmail.com>
2026-06-20 12:35:21 -0600

    fix nesting

--- bookings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import booking_form, success
-
-urlpatterns = [
-    path('', booking_form, name='booking_form'),
-    path('success/', success, name='success'),
-]
