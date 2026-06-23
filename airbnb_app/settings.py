c5dab52d6713d32c87d125b9ec1a33d09d3178b7
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-23 23:16:17 +0800

    debug fixed

--- airbnb_app/settings.py
+++ airbnb_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-91@7ckx654ju$lb=-pv4y1jefn@2_-^2r+&4$ij$8860-o^ig&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["airbnbapp-production.up.railway.app",
     "localhost",
