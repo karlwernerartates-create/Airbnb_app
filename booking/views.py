54216f9cfbd782d0314c83246817ab65b6c10a35
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-26 21:37:15 +0800

    another fixed

--- booking/views.py
+++ booking/views.py
@@ -6,30 +6,13 @@ from .email_service import send_admin_notification
 def create_booking(request):
     properties = Property.objects.all()
 
-    print("Properties:", list(properties.values()))
-    print("Count:", properties.count())
-
-    if request.method == "POST":
-        email = request.POST.get("email")
-        property_id = request.POST.get("property_id")
-
-        property_obj = get_object_or_404(Property, id=property_id)
-
-        booking = Booking.objects.create(
-            email=email,
-            property=property_obj
-        )
-
-        send_admin_notification(booking)
-
-        return redirect("success")
-
     return render(
         request,
         "booking/create_booking.html",
         {
-            "properties": properties
-        }
+            "properties": properties,
+            "debug": list(properties.values()),
+        },
     )
 
 
