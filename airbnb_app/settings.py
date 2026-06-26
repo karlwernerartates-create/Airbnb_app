50bc046664cb8da70da1c4d79959a6d3d372f7e2
Karl Werner Artates <karlwernerartates@gmail.com>
2026-06-26 22:16:48 +0800

    Use PostgreSQL

--- airbnb_app/settings.py
+++ airbnb_app/settings.py
@@ -80,12 +80,13 @@ WSGI_APPLICATION = 'airbnb_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
